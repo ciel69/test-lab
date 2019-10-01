7846f5049fd7b6dda594ec6683b927c4310fd8d2
Aleksei Bukhaltsov <Bukhaltsov@lanit.ru>
2019-10-01 12:12:01 +0300

    Updates

---
+++ precache-manifest.1a3212c9d83e8a905ecd8f9cc846cedb.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "827972cf645b890eb0286ab5d5d6e636",
+    "url": "/test-lab/index.html"
+  },
+  {
+    "revision": "21588e9c37a6bae0d06d",
+    "url": "/test-lab/static/css/2.4c33dc2c.chunk.css"
+  },
+  {
+    "revision": "9e5d5a7f7d2fdecce5f7",
+    "url": "/test-lab/static/css/main.4c0a1749.chunk.css"
+  },
+  {
+    "revision": "21588e9c37a6bae0d06d",
+    "url": "/test-lab/static/js/2.4fafa0c8.chunk.js"
+  },
+  {
+    "revision": "9e5d5a7f7d2fdecce5f7",
+    "url": "/test-lab/static/js/main.9d43a35c.chunk.js"
+  },
+  {
+    "revision": "1bb64b83b7c91c6a0c19",
+    "url": "/test-lab/static/js/runtime-main.ceee7b5b.js"
+  },
+  {
+    "revision": "1f704893145d21747186116f85b2c062",
+    "url": "/test-lab/static/media/ArialBlack.1f704893.ttf"
+  },
+  {
+    "revision": "4212212c3bf6d0c0904bd15957fc0ee1",
+    "url": "/test-lab/static/media/ArialBlack.4212212c.woff"
+  },
+  {
+    "revision": "ed14e665ff1adc2ea20f7f8910a92c93",
+    "url": "/test-lab/static/media/ArialBlack.ed14e665.eot"
+  },
+  {
+    "revision": "1114c72e10775b3252e4f1de157e2f70",
+    "url": "/test-lab/static/media/ArialBold.1114c72e.woff"
+  },
+  {
+    "revision": "33659c3ee1590a4826de9a70b48fb502",
+    "url": "/test-lab/static/media/ArialBold.33659c3e.ttf"
+  },
+  {
+    "revision": "c2f15177002cc914cd9563ccda457343",
+    "url": "/test-lab/static/media/ArialBold.c2f15177.eot"
+  },
+  {
+    "revision": "325231ced15078266604869f333a0dc4",
+    "url": "/test-lab/static/media/ArialBoldItalic.325231ce.woff"
+  },
+  {
+    "revision": "83d27d61b1b84da7eba082ff5ece92d4",
+    "url": "/test-lab/static/media/ArialBoldItalic.83d27d61.eot"
+  },
+  {
+    "revision": "e9e3cdb926753ee01b398d1820d7499c",
+    "url": "/test-lab/static/media/ArialBoldItalic.e9e3cdb9.ttf"
+  },
+  {
+    "revision": "0cac7d1a894ee4dc31c0ab41964ae9f7",
+    "url": "/test-lab/static/media/ArialItalic.0cac7d1a.ttf"
+  },
+  {
+    "revision": "8f4fab420e5786823188ccc4dca5bd5d",
+    "url": "/test-lab/static/media/ArialItalic.8f4fab42.woff"
+  },
+  {
+    "revision": "b574b13888f4ac211769b800b83f953d",
+    "url": "/test-lab/static/media/ArialItalic.b574b138.eot"
+  },
+  {
+    "revision": "3c9390eb37dd4ab8c670ecd71feb7f71",
+    "url": "/test-lab/static/media/ArialRegular.3c9390eb.ttf"
+  },
+  {
+    "revision": "b9671ad265339e15124b8e3827a3af9d",
+    "url": "/test-lab/static/media/ArialRegular.b9671ad2.woff"
+  },
+  {
+    "revision": "d50bde08d8f4127daac7075a9d7c4251",
+    "url": "/test-lab/static/media/ArialRegular.d50bde08.eot"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/test-lab/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/test-lab/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/test-lab/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/test-lab/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  },
+  {
+    "revision": "69e843a01470f85c583a5ca72a59c807",
+    "url": "/test-lab/static/media/bg-header.69e843a0.png"
+  },
+  {
+    "revision": "9deaffbb056f3c5b14a93a6a9880624b",
+    "url": "/test-lab/static/media/close.9deaffbb.svg"
+  },
+  {
+    "revision": "4a3cf18340c1dd64be22c8870ae29e44",
+    "url": "/test-lab/static/media/icon.4a3cf183.svg"
+  },
+  {
+    "revision": "1395b5c9eb9dc2124642087216ff1d6d",
+    "url": "/test-lab/static/media/pc.1395b5c9.png"
+  }
+]);
--- src/store/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as form } from 'redux-form';
-
-const reducers = {
-  form, // the state and validation of form fields
-};
-
-export default combineReducers(reducers);
